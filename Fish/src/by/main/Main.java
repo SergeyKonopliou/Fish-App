f04250e71c20f8355691a5b727298e3dce7118ad
SergeyKonopliou <joker_te@mail.ru>
2021-05-31 09:27:01 +0300

    version1

---
+++ Fish/src/by/main/Main.java
@@ -0,0 +1,20 @@
+package by.main;
+
+import javax.swing.SwingUtilities;
+
+import by.view.PanelMain;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new PanelMain().createFrame();
+			}
+		});
+	}
+	
+	
+	
+}
